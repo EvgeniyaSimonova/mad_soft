59d813e016e451fdd351dbfa6c398b3191b06fed
Evgeniya_Simonova <simonova2001-2@yandex.ru>
2024-04-16 15:37:30 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mad_soft/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mad_soft
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mad_soft/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
